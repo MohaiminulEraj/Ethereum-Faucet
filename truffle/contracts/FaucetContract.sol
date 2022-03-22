c5d8d30604d72c792730edb50e7de2bb4b58b3f4
MohaiminulEraj <eraj471@gmail.com>
2022-03-22 18:12:51 +0600

    funders mapping added to Faucet Contract

--- truffle/contracts/FaucetContract.sol
+++ truffle/contracts/FaucetContract.sol
@@ -2,8 +2,31 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
-    // storage variables
-    uint256 public funds = 1000;
-    int256 public counter = -10;
-    uint32 public test = 4294967295;
+    // storing address of all the users that are using addFunds()
+    uint256 public numOffFunders;
+    mapping(uint256 => address) private funders;
+
+    receive() external payable {}
+
+    function addFunds() external payable {
+        uint256 index = numOffFunders++;
+        funders[index] = msg.sender;
+    }
+
+    function getAllFunders() external view returns (address[] memory) {
+        address[] memory _funders = new address[](numOffFunders);
+        for (uint256 i = 0; i < numOffFunders; i++) {
+            _funders[i] = funders[i];
+        }
+        return _funders;
+    }
+
+    function getFunderAtIndex(uint8 index) external view returns (address) {
+        return funders[index];
+    }
 }
+
+// const { ethers } = require('ethers');
+// const instance = await Faucet.deployed();
+// instance.addFunds({from: accounts[0], value: ethers.utils.parseEther('1')});
+// instance.addFunds({from: accounts[1], value: ethers.utils.parseEther('1')});
