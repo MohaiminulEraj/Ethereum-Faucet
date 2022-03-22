7f9fa62fb00c39fa2c0a393af715e27b0ec1ab09
MohaiminulEraj <eraj471@gmail.com>
2022-03-22 18:23:17 +0600

    prevented duplications for funders

--- truffle/contracts/FaucetContract.sol
+++ truffle/contracts/FaucetContract.sol
@@ -4,13 +4,16 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
     // storing address of all the users that are using addFunds()
     uint256 public numOffFunders;
-    mapping(uint256 => address) private funders;
+    mapping(address => bool) private funders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        uint256 index = numOffFunders++;
-        funders[index] = msg.sender;
+        address funder = msg.sender;
+        if (!funders[funder]) {
+            numOffFunders++;
+            funders[funder] = true;
+        }
     }
 
     function getAllFunders() external view returns (address[] memory) {
