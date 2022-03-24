4150673e55ba6938f1012be169873915c8cebc68
MohaiminulEraj <eraj471@gmail.com>
2022-03-24 23:39:14 +0600

    MetaMask connect & getBalance() working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,80 +1,94 @@
 import React, { useState, useEffect } from 'react';
 import "./App.css";
-import { ethers } from "ethers";
+import { ethers, Contract } from "ethers";
+import detectEthereumProvider from '@metamask/detect-provider';
+// import { loadContract } from "./utils/load-contract";
+// import loadContract from "../public/contracts/Faucet.json";
+import loadFaucetABI from "./contracts/Faucet.json";
+// import dotenv from 'dotenv';
+// // parsing .env file
+// dotenv.config()
 
 const App = () => {
 
   const [web3Api, setWeb3Api] = useState({
     provider: null,
-    web3: null
+    web3: null,
+    contract: null,
   });
-
+  const ethersProvider = new ethers.providers.Web3Provider(window.ethereum);
+  // console.log(ethersProvider)
   const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
 
   useEffect(() => {
     const loadProvider = async () => {
       // let provider = ethers.getDefaultProvider("ropsten");
-      let provider = null;
-      if (window.ethereum) {
-        provider = window.ethereum;
+      const provider = await detectEthereumProvider();
+      // const contract = await loadContract("Faucet");
+      const contract = loadFaucetABI
+      // console.log('contract', contract)
+      // let c = new Contract(account, contract, provider);
+      // console.log('c', c)
 
-        console.log(provider)
-        try {
-          await provider.enable();
-        } catch {
-          console.error("User denied accounts access!");
-        }
+      if (provider) {
+        provider.request({ method: "eth_requestAccounts" });
+        setWeb3Api({
+          web3: new ethers.providers.Web3Provider(provider),
+          provider,
+          contract
+        });
+      } else {
+        console.error('Please Install MetaMask!');
       }
-      else if (window.web3) {
-        provider = window.web3.currentProvider;
-      }
-      else if (!process.env.production) {
-        provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-      }
-      setWeb3Api({
-        // web3: new ethers.getDefaultProvider(provider),
-        web3: new ethers.providers.Web3Provider(provider),
-        provider
-      })
     }
     loadProvider();
   }, []);
 
   useEffect(() => {
     const getAccount = async () => {
+      console.log(web3Api.provider);
       if (web3Api.provider) {
-        let accounts = await web3Api.provider.request({ method: "eth_requestAccounts" });
+        // let accounts = await web3Api.provider.request({ method: "eth_requestAccounts" });
+        let accounts = await ethersProvider.listAccounts();;
         setAccount(accounts[0]);
-        console.log(accounts[0]);
+        setBalance(ethers.utils.formatEther(await ethersProvider.getBalance(accounts[0])).toString())
       }
+      // let c = new Contract(process.env.CONTRACT_ADDRESS, contract, ethersProvider);
+      // console.log(((await c.provider.getBalance(process.env.CONTRACT_ADDRESS)).toNumber()))
     }
     web3Api.provider && getAccount();
   }, [web3Api.provider]);
+  const contract = loadFaucetABI
+  // console.log('contract', contract)
+  // console.log()
 
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
-          <span>
-            <strong>Account: </strong>
-          </span>
-          <h1>
-            {account ? account : 'Not Connected!'}
-          </h1>
-          <div className="balance-view is-size-2">
-            Current Banalce: <strong>0</strong> ETH
+          <div className="is-flex is-align-items-center">
+            <span>
+              <strong className="mr-2">Account: </strong>
+            </span>
+            {
+              account ?
+                account :
+                <button className="button is-small"
+                  onClick={() => {
+                    web3Api.provider.request({ method: "eth_requestAccounts" });
+                  }}
+                >
+                  Connect Wallet
+                </button>
+            }
           </div>
-          {!account &&
-            <button className="btn mr-2"
-              onClick={() => {
-                window.location.reload();
-              }}
-            >
-              Connect Account
-            </button>
-          }
-          <button className="btn mr-2">Donate</button>
-          <button className="btn">Withdraw</button>
+
+          <div className="balance-view is-size-2 my-4">
+            Current Banalce: <strong>{balance ? balance.slice(0, 6) : 0}</strong> ETH
+          </div>
+          <button className="button is-link mr-2">Donate</button>
+          <button className="button is-primary">Withdraw</button>
         </div>
       </div>
     </>
